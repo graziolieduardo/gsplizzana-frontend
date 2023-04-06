c94b10afd7a020992c961a50985465289333fa1d
Eduardo <eduardo@pigmentolab.com>
2023-04-06 20:58:09 +0200

    create layout comp

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Header } from '../partials/Header'
+import { Footer } from '../partials/Footer'
+
+type LayoutProps = {
+    children: React.ReactNode
+}
+
+export const Layout = ({ children }: LayoutProps) => {
+    return (
+        <>
+            {/* <Header /> */}
+            <main>
+                {children}
+            </main>
+            {/* <Footer /> */}
+        </>
+    )
+}
