f0c90114ca730bf805e6656c6f9c5c18cba9d031
Eduardo <eduardo@pigmentolab.com>
2023-04-06 21:36:01 +0200

    install tailwind and move folders

---
+++ src/pages/iscrizioni/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const index = () => {
+    return (
+        <>
+            <div>
+                {/* descrizione della pagina */}
+            </div>
+            <form action="">
+            ciao
+            </form>
+        </>
+    )
+}
+
+export default index
