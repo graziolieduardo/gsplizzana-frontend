a1078428daa8940875496edc524638376754d823
Eduardo <eduardo@pigmentolab.com>
2023-04-15 17:44:52 +0200

    update meta tags

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Header } from '../partials/Header'
 import { Footer } from '../partials/Footer'
+import Head from 'next/head'
 
 type LayoutProps = {
     children: React.ReactNode
@@ -8,12 +9,20 @@ type LayoutProps = {
 
 export const Layout = ({ children }: LayoutProps) => {
     return (
-        <div className='h-screen flex flex-col'>
-            <Header />
-            <main className='flex-1'>
-                {children}
-            </main>
-            <Footer />
-        </div>
+        <>
+            <Head>
+                <title>GSP Lizzana</title>
+                <meta name="description" content="Il Mondiale si gioca sul gonfiabile" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link rel="icon" href="/static/logo-arancione.svg" />
+            </Head>
+            <div className='h-screen flex flex-col'>
+                <Header />
+                <main className='flex-1'>
+                    {children}
+                </main>
+                <Footer />
+            </div>
+        </>
     )
 }
