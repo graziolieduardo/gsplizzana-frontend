ccabc3e538a2cba43daa717a4a2628d4fb3b3d9f
Eduardo <eduardo@pigmentolab.com>
2023-04-17 14:56:01 +0200

    fix

--- src/pages/iscrizioni/confirmation.tsx
+++ src/pages/iscrizioni/confirmation.tsx
@@ -10,9 +10,16 @@ export default function Confirmation() {
                 <div className="w-14 h-14 rounded-full flex justify-center items-center bg-gradient-to-r from-primary-dark to-primary text-white">&#10003;</div>
 
                 <h2 className="text-3xl font-bold mt-6">Pre-iscrizione inviata</h2>
-                <p className="text-secondary text-center mt-4 font-light">Il tuo numero di iscrizione è: <span className="font-bold">{token}</span></p>
-                <p className="text-secondary text-center mt-4 font-light">Ricordati di scaricare <span className="font-bold underline hover:text-primary-dark"><Link href={"https://pdfhost.io/v/Pxsk.zjwf_Liberatoria_GDPR_GSP"} target="blank">qui</Link></span> i moduli per la privacy e portali compilati il giorno dell&apos;iscrizione.</p>
+
+                <div className="mt-4">
+                    <p className="text-secondary text-center font-light">Il tuo numero di iscrizione è: <span className="font-bold">{token}</span></p>
+                </div>
+
+                <p className="text-secondary text-center mt-4 font-light">Ricordati di scaricare <span className="font-bold underline hover:text-primary-dark"><Link href={"https://pdfhost.io/v/Pxsk.zjwf_Liberatoria_GDPR_GSP"} target="blank">qui</Link></span> i moduli per la privacy e di portarli compilati.</p>
+
                 <p className="text-secondary text-center mt-4 font-light">Ti aspettiamo a Lizzana il <span className="font-bold">29 aprile ore 7.00</span> per completare l&apos;iscrizione.</p>
+
+                <p className="text-secondary text-center mt-4 font-light">Per qualsiasi problema o dubbio siamo a disposizione.</p>
 
                 {/* <div className="mt-8 w-4/5">
                     <button className="block mx-auto rounded-full bg-gradient-to-r w-full lg:w-1/2 from-primary-dark to-primary py-2 text-white" >Chiudi</button>
