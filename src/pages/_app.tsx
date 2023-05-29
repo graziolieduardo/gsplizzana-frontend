9c5c0ffceacdaec78cdab81a1d617f1d8e30bf41
Leograzioli <leograzioli@gmail.com>
2023-05-29 14:19:47 +0200

    add Inter font style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,19 @@ import '@/src/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SignupContextProvider } from '../contexts/SignupContext';
 
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <SignupContextProvider>
             <Layout>
+                <style jsx global>{`
+                        html {
+                        font-family: ${inter.style.fontFamily};
+                        }`}
+                </style>
                 <Component {...pageProps} />
             </Layout>
         </SignupContextProvider>
