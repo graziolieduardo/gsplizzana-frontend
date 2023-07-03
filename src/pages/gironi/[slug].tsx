a8d2352b1a7b5077dc41469b5687e6ad4556cfee
Leograzioli <leograzioli@gmail.com>
2023-07-03 14:42:01 +0200

    fix gironi page push

--- src/pages/gironi/[slug].tsx
+++ src/pages/gironi/[slug].tsx
@@ -21,7 +21,7 @@ const index = () => {
       <section className="px-4 max-w-container 2xl:max-w-container-xl mx-auto">
 
         {/* page title  */}
-        <div className="flex gap-x-2 pt-24">
+        <div className="flex gap-x-2 pt-24 pb-12">
           <div className="w-1/2">
             <Title variant={Title.variant.primary} > Scopri i <span className="text-primary">gironi</span> del torneo</Title>
             <p className="mt-8">E anche quest’anno per il torneo maschile ci sono 8 giorni da 4 squadre ciascuno e per il torneo femminile ci sono 4 gironi da 4 squadre. In aggiunta è stato organizzato anche un girone Rookie per agevolare le squadre new entry.</p>
@@ -31,10 +31,10 @@ const index = () => {
         </div>
 
         {/* maschile and feminile switch */}
-        <div className="pt-24" id="tables">
+        <div className="pt-12">
           <div className="flex justify-around border-2 rounded border-primary-dark">
-            <div onClick={() => { setActivePage('maschile') }} className={`font-semibold ${activePage === "maschile" ? 'bg-gradient-to-r from-primary-dark to-primary text-white' : 'bg-white text-black'} w-1/2 text-center py-2`} >Maschili</div>
-            <div onClick={() => { setActivePage('feminile') }} className={`font-semibold ${activePage === "feminile" ? 'bg-gradient-to-r from-primary-dark to-primary text-white' : 'bg-white text-black'}  w-1/2 text-center py-2 `} >Feminili</div>
+            <div onClick={() => { router.push('/gironi/maschile') }} className={`font-semibold ${activePage === "maschile" ? 'bg-gradient-to-r from-primary-dark to-primary text-white' : 'bg-white text-black'} w-1/2 text-center py-2`} >Maschili</div>
+            <div onClick={() => { router.push('/gironi/feminile') }} className={`font-semibold ${activePage === "feminile" ? 'bg-gradient-to-r from-primary-dark to-primary text-white' : 'bg-white text-black'}  w-1/2 text-center py-2 `} >Feminili</div>
           </div>
         </div>
 
