af7aaf544acdbd1fd7bcf6ba97fbd443dac905a5
Eduardo <eduardo@pigmentolab.com>
2023-08-13 15:21:02 +0200

    fix

--- src/components/common/SemifinaleFBracket.tsx
+++ src/components/common/SemifinaleFBracket.tsx
@@ -9,23 +9,23 @@ export const SemifinaleFBracket = ({ matches }: any) => {
 
                     <div className="flex justify-between py-4 px-2">
                         <div className="flex items-center gap-x-2">
-                            {matches[4]?.home_team.length > 0 ? <img width={40} src={`/static/loghi_squadre/tondi/TEAM/${matches[4]?.home_team?.name?.replace(/\s/g, '').replace('#', '').replace('-', '').replace('\'', '').replace('(', '').replace(')', '').toLowerCase()}.png`} alt="" /> : <img src="/static/ellipse_2.svg" alt="" />}
-                            <p>{matches[4]?.home_team.length > 0 ? matches[4]?.home_team.name : 'Squadra da definire'}</p>
+                            {matches[4]?.home_team ? <img width={40} src={`/static/loghi_squadre/tondi/TEAM/${matches[4]?.home_team?.name?.replace(/\s/g, '').replace('#', '').replace('-', '').replace('\'', '').replace('(', '').replace(')', '').toLowerCase()}.png`} alt="" /> : <img src="/static/ellipse_2.svg" alt="" />}
+                            <p>{matches[4]?.home_team ? matches[4]?.home_team.name : 'Squadra da definire'}</p>
                         </div>
                         <p>{(matches[4]?.closed || matches[4]?.live) ? matches[4]?.home_team.score : null}</p>
                     </div>
 
                     <div className="flex justify-between py-4 px-2">
                         <div className="flex items-center gap-x-2">
-                            {matches[4]?.away_team.length > 0 ? <img width={40} src={`/static/loghi_squadre/tondi/TEAM/${matches[4]?.away_team?.name?.replace(/\s/g, '').replace('#', '').replace('-', '').replace('\'', '').replace('(', '').replace(')', '').toLowerCase()}.png`} alt="" /> : <img src="/static/ellipse_2.svg" alt="" />}
-                            <p>{matches[4]?.away_team.length > 0 ? matches[4]?.away_team.name : 'Squadra da definire'}</p>
+                            {matches[4]?.away_team ? <img width={40} src={`/static/loghi_squadre/tondi/TEAM/${matches[4]?.away_team?.name?.replace(/\s/g, '').replace('#', '').replace('-', '').replace('\'', '').replace('(', '').replace(')', '').toLowerCase()}.png`} alt="" /> : <img src="/static/ellipse_2.svg" alt="" />}
+                            <p>{matches[4]?.away_team ? matches[4]?.away_team.name : 'Squadra da definire'}</p>
                         </div>
                         <p>{(matches[4]?.closed || matches[4]?.live) ? matches[4]?.away_team.score : null}</p>
                     </div>
                 </div>
                 <div className="border-t">
                     <div className="p-4 text-center">
-                        {matches[4]?.away_team.length > 0 ? <Link href={`/partita/${matches[4]?.id}`} className="text-primary-dark text-sm font-semibold">Dettagli partita</Link> : null}
+                        {matches[4]?.away_team ? <Link href={`/partita/${matches[4]?.id}`} className="text-primary-dark text-sm font-semibold">Dettagli partita</Link> : null}
                     </div>
                 </div>
             </div>
@@ -35,23 +35,23 @@ export const SemifinaleFBracket = ({ matches }: any) => {
 
                     <div className="flex justify-between py-4 px-2">
                         <div className="flex items-center gap-x-2">
-                            {matches[5]?.home_team.length > 0 ? <img width={40} src={`/static/loghi_squadre/tondi/TEAM/${matches[5]?.home_team?.name?.replace(/\s/g, '').replace('#', '').replace('-', '').replace('\'', '').replace('(', '').replace(')', '').toLowerCase()}.png`} alt="" /> : <img src="/static/ellipse_2.svg" alt="" />}
-                            <p>{matches[5]?.home_team.length > 0 ? matches[5]?.home_team.name : 'Squadra da definire'}</p>
+                            {matches[5]?.home_team ? <img width={40} src={`/static/loghi_squadre/tondi/TEAM/${matches[5]?.home_team?.name?.replace(/\s/g, '').replace('#', '').replace('-', '').replace('\'', '').replace('(', '').replace(')', '').toLowerCase()}.png`} alt="" /> : <img src="/static/ellipse_2.svg" alt="" />}
+                            <p>{matches[5]?.home_team ? matches[5]?.home_team.name : 'Squadra da definire'}</p>
                         </div>
                         <p>{(matches[5]?.closed || matches[5]?.live) ? matches[5]?.home_team.score : null}</p>
                     </div>
 
                     <div className="flex justify-between py-4 px-2">
                         <div className="flex items-center gap-x-2">
-                            {matches[5]?.away_team.length > 0 ? <img width={40} src={`/static/loghi_squadre/tondi/TEAM/${matches[5]?.away_team?.name?.replace(/\s/g, '').replace('#', '').replace('-', '').replace('\'', '').replace('(', '').replace(')', '').toLowerCase()}.png`} alt="" /> : <img src="/static/ellipse_2.svg" alt="" />}
-                            <p>{matches[5]?.away_team.length > 0 ? matches[5]?.away_team.name : 'Squadra da definire'}</p>
+                            {matches[5]?.away_team ? <img width={40} src={`/static/loghi_squadre/tondi/TEAM/${matches[5]?.away_team?.name?.replace(/\s/g, '').replace('#', '').replace('-', '').replace('\'', '').replace('(', '').replace(')', '').toLowerCase()}.png`} alt="" /> : <img src="/static/ellipse_2.svg" alt="" />}
+                            <p>{matches[5]?.away_team ? matches[5]?.away_team.name : 'Squadra da definire'}</p>
                         </div>
                         <p>{(matches[5]?.closed || matches[5]?.live) ? matches[5]?.away_team.score : null}</p>
                     </div>
                 </div>
                 <div className="border-t">
                     <div className="p-4 text-center">
-                        {matches[5]?.away_team.length > 0 ? <Link href={`/partita/${matches[5]?.id}`} className="text-primary-dark text-sm font-semibold">Dettagli partita</Link> : null}
+                        {matches[5]?.away_team ? <Link href={`/partita/${matches[5]?.id}`} className="text-primary-dark text-sm font-semibold">Dettagli partita</Link> : null}
                     </div>
                 </div>
             </div>
