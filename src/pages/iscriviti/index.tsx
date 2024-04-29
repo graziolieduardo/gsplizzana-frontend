ca8f9211bb18987a5dc533281152a8c81fe8a22b
Leograzioli <leograzioli@gmail.com>
2024-04-29 12:21:31 +0200

    fix

--- src/pages/iscriviti/index.tsx
+++ src/pages/iscriviti/index.tsx
@@ -27,7 +27,7 @@ export default function Login() {
                             <label htmlFor="name" className="block font-semibold ">Nome</label>
                             <input{...register("name", {
                                 required: '- this field is required' 
-                            })} type="text" id="name" className={ `${errors.name? 'focus:outline-red-500 border-2 border-red-500' : ''} rounded bg-gray-100 px-2 py-1 w-full focus:outline-primary`} placeholder="Luigi" />
+                            })} type="text" id="name" className={ `${errors.name? 'focus:outline-red-500 border-2 border-red-500' : 'focus:outline-primary'} rounded bg-gray-100 px-2 py-1 w-full`} placeholder="Luigi" />
                             {errors.name && <div className="text-xs text-red-500">{errors.name.message as string}</div> }
                         </div>
 
@@ -36,7 +36,7 @@ export default function Login() {
                             <label htmlFor="surname" className="block font-semibold mt-4 md:mt-0">Cognome</label>
                             <input{...register("surname", {
                                 required: '- the field surname is required' 
-                            })} type="text" id="surname" className={ `${errors.surname? 'focus:outline-red-500 border-2 border-red-500' : ''} rounded bg-gray-100 px-2 py-1 w-full focus:outline-primary`} placeholder="Buffon" />
+                            })} type="text" id="surname" className={ `${errors.surname? 'focus:outline-red-500 border-2 border-red-500' : 'focus:outline-primary'} rounded bg-gray-100 px-2 py-1 w-full`} placeholder="Buffon" />
                             {errors.surname && <div className="text-xs text-red-500">{errors.surname.message as string}</div> }
                         </div>
                     </div>
@@ -45,21 +45,21 @@ export default function Login() {
                         {/* email */}
                         <div>
                             <label htmlFor="email" className="block font-semibold mt-4 md:mt-0">Email</label>
-                            <input{...register("email", {required: '- the field email is required', pattern: {value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, message: '- Scegli un email valido'} })} type="text" id="email" className={ `${errors.email? 'focus:outline-red-500 border-2 border-red-500' : ''} rounded bg-gray-100 px-2 py-1 w-full focus:outline-primary`} placeholder="luigibuffon@gmail.com" />
+                            <input{...register("email", {required: '- the field email is required', pattern: {value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, message: '- Scegli un email valido'} })} type="text" id="email" className={ `${errors.email? 'focus:outline-red-500 border-2 border-red-500' : 'focus:outline-primary'} rounded bg-gray-100 px-2 py-1 w-full`} placeholder="luigibuffon@gmail.com" />
                             {errors.email && <div className="text-xs text-red-500">{errors.email.message as string}</div> }
                         </div>
 
                         {/* Username */}
                         <div>
                             <label htmlFor="username" className="block font-semibold mt-4 md:mt-0">Username</label>
-                            <input{...register("username", {required: '- the field username is required'})} type="text" id="username" className={ `${errors.username? 'focus:outline-red-500 border-2 border-red-500' : ''} rounded bg-gray-100 px-2 py-1 w-full focus:outline-primary`} placeholder="luiginho" />
+                            <input{...register("username", {required: '- the field username is required'})} type="text" id="username" className={ `${errors.username? 'focus:outline-red-500 border-2 border-red-500' : 'focus:outline-primary'} rounded bg-gray-100 px-2 py-1 w-full`} placeholder="luiginho" />
                             {errors.username && <div className="text-xs text-red-500">{errors.username.message as string}</div> }
                         </div>
 
                         {/* Birthdate */}
                         <div>
                             <label htmlFor="birthdate" className="block font-semibold mt-4 md:mt-0">Birthdate</label>
-                            <input{...register("birthdate", {required: '- Scegli una data'})} type="date" id="birthdate" className={ `${errors.birthdate? 'focus:outline-red-500 border-2 border-red-500' : ''} rounded bg-gray-100 px-2 py-1 w-full focus:outline-primary`} placeholder="birthdate" />
+                            <input{...register("birthdate", {required: '- Scegli una data'})} type="date" id="birthdate" className={ `${errors.birthdate? 'focus:outline-red-500 border-2 border-red-500' : 'focus:outline-primary'} rounded bg-gray-100 px-2 py-1 w-full`} placeholder="birthdate" />
                             {errors.birthdate && <div className="text-xs text-red-500">{errors.birthdate.message as string}</div> }
                         </div>
                     </div>
@@ -69,14 +69,14 @@ export default function Login() {
                         {/* password */}
                         <div className="flex-1">
                             <label htmlFor="password" className="block font-semibold mt-4 md:mt-0">Password</label>
-                            <input{...register("password", {required: '- scegli una password'})} type="password" id="password" className={ `${(errors.password || errors.password_confirmation?.message)? 'focus:outline-red-500 border-2 border-red-500' : ''} rounded bg-gray-100 px-2 py-1 w-full focus:outline-primary`} placeholder="password" />
+                            <input{...register("password", {required: '- scegli una password'})} type="password" id="password" className={ `${(errors.password || errors.password_confirmation?.message)? 'focus:outline-red-500 border-2 border-red-500' : 'focus:outline-primary'} rounded bg-gray-100 px-2 py-1 w-full`} placeholder="password" />
                             {errors.password && <div className="text-xs text-red-500">{errors.password.message as string}</div> }
                         </div>
 
                         {/* confirm password */}
                         <div className="flex-1">
                             <label htmlFor="password_confirmation" className="block font-semibold mt-4 md:mt-0">Conferma Password</label>
-                            <input{...register("password_confirmation", {required: '- rimeti la password',  validate: value => value === watch('password') || '- Le password sono diverse'})} type="password" id="password_confirmation" className={ `${errors.password_confirmation? 'focus:outline-red-500 border-2 border-red-500' : ''} rounded bg-gray-100 px-2 py-1 w-full focus:outline-primary`} placeholder="confirm-password" />
+                            <input{...register("password_confirmation", {required: '- rimeti la password',  validate: value => value === watch('password') || '- Le password sono diverse'})} type="password" id="password_confirmation" className={ `${errors.password_confirmation? 'focus:outline-red-500 border-2 border-red-500' : 'focus:outline-primary'} rounded bg-gray-100 px-2 py-1 w-full`} placeholder="confirm-password" />
                             {errors.password_confirmation && <div className="text-xs text-red-500">{errors.password_confirmation.message as string}</div> }
 
                         </div>
