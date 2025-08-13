fa00b08b12418ee3cf6f5de0b7ca05a6345ca3d2
Eduardo <eduardo@pigmentolab.com>
2025-08-13 14:51:57 +0200

    update programma 2025

--- src/components/common/Programma.tsx
+++ src/components/common/Programma.tsx
@@ -3,98 +3,32 @@ import { Title } from "./Title";
 
 export default function Programma() {
     const program: any = {
-        mercoledì: [
-            {
-                weekDay: 'mercoledì',
-                day: '14.08'
-            },
-            {
-                title: 'Inizio torneo calciosplash️',
-                time: '16.00',
-                important: true,
-                highlight: true
-            },
-            // {
-            //     title: 'Inizio Beer Pong',
-            //     time: '17.00',
-            //     important: false,
-            //     highlight: false
-            // },
-            {
-                title: 'Pausa cena',
-                time: '20.00',
-                important: false,
-                highlight: false
-            },
-            {
-                title: 'Desva',
-                time: '20.00',
-                important: false,
-                highlight: true
-            },
-            {
-                title: 'Ripresa partite',
-                time: '21.00',
-                important: false,
-                highlight: false
-            },
-            // {
-            //     title: 'Fine Beer Pong',
-            //     time: '23.00',
-            //     important: false,
-            //     highlight: false
-            // },
-            {
-                title: 'Fine partite',
-                time: '24.00',
-                important: false,
-                highlight: false
-            },
-            {
-                title: 'Pablo Norex',
-                time: '00.20',
-                important: false,
-                highlight: true
-            },
-            {
-                title: 'Villa People X Ebol4',
-                time: '00.40',
-                important: false,
-                highlight: true
-            },
-            {
-                title: 'Chiusura',
-                time: '1.30',
-                important: false,
-                highlight: true
-            }
-        ],
         giovedì: [
             {
                 weekDay: 'giovedì',
-                day: '15.08'
+                day: '14.08'
             },
             {
                 title: 'Inizio partite',
-                time: '15.00',
+                time: '16.00',
                 important: false,
                 highlight: false
             },
             {
-                title: 'Inizio Beer Pong',
+                title: 'Beer Pong',
                 time: '19.00',
                 important: false,
                 highlight: false
             },
             {
-                title: 'Pausa cena',
+                title: 'LEDWALL SHOW - Pera',
                 time: '20.00',
                 important: false,
                 highlight: false
             },
             {
-                title: 'Gara alette',
-                time: '20.00',
+                title: 'LEDWALL SHOW - Carpa',
+                time: '20.30',
                 important: false,
                 highlight: true
             },
@@ -105,26 +39,20 @@ export default function Programma() {
                 highlight: false
             },
             {
-                title: 'Fine partite',
-                time: '24.00',
+                title: 'Dj set - Cellar Door',
+                time: '23.00',
                 important: false,
                 highlight: false
             },
-            // {
-            //     title: 'Fine Beer Pong',
-            //     time: '23.00',
-            //     important: false,
-            //     highlight: false
-            // },
             {
-                title: 'Karaoke',
-                time: '00.20',
+                title: 'Dj cap',
+                time: '00.00',
                 important: false,
-                highlight: true
+                highlight: false
             },
             {
                 title: 'Chiusura',
-                time: '1.30',
+                time: '01.00',
                 important: false,
                 highlight: true
             }
@@ -132,31 +60,25 @@ export default function Programma() {
         venerdì: [
             {
                 weekDay: 'venerdì',
-                day: '16.08'
+                day: '15.08'
             },
             {
                 title: 'Inizio partite',
-                time: '16.00',
+                time: '11.00',
                 important: false,
                 highlight: false
             },
             {
-                title: 'Inizio Beer Pong',
+                title: 'Beer Pong',
                 time: '19.00',
                 important: false,
                 highlight: false
             },
             {
-                title: 'Pausa cena',
+                title: 'LEDWALL SHOW - Gremps',
                 time: '20.00',
                 important: false,
                 highlight: false
-            },
-            {
-                title: 'Bao di sera',
-                time: '20.00',
-                important: false,
-                highlight: true
             },
             {
                 title: 'Ripresa partite',
@@ -164,33 +86,15 @@ export default function Programma() {
                 important: false,
                 highlight: false
             },
-            // {
-            //     title: 'Pausa cena + Gremps',
-            //     time: '20.00',
-            //     important: false,
-            //     highlight: true
-            // },
-            // {
-            //     title: 'Ripresa partite',
-            //     time: '21.00',
-            //     important: false,
-            //     highlight: false
-            // },
             {
-                title: 'Fine partite',
-                time: '22.40',
-                important: false,
-                highlight: false
-            },
-            {
-                title: 'Super saom league ✨',
+                title: 'Super saom show ✨',
                 time: '23.00',
                 important: true,
                 highlight: true
             },
             {
                 title: 'Chiusura',
-                time: '1.30',
+                time: '01.00',
                 important: false,
                 highlight: true
             }
@@ -198,71 +102,53 @@ export default function Programma() {
         sabato: [
             {
                 weekDay: 'sabato',
-                day: '17.08'
+                day: '16.08'
             },
             {
                 title: 'Inizio partite',
-                time: '10.00',
+                time: '12.20',
                 important: false,
                 highlight: false
             },
             {
-                title: 'Pausa pranzo',
-                time: '14.00',
-                important: false,
-                highlight: false
-            },
-            {
-                title: 'Ripresa partite',
-                time: '15.00',
-                important: false,
-                highlight: false
-            },
-            {
-                title: 'Inizio Beer Pong',
+                title: 'Beer Pong',
                 time: '19.00',
                 important: false,
                 highlight: false
             },
             {
-                title: 'Pausa cena',
-                time: '20.00',
-                important: false,
-                highlight: false
-            },
-            {
-                title: 'GREMPS',
+                title: 'LEDWALL SHOW - Grabbbrielll',
                 time: '20.00',
                 important: false,
                 highlight: true
             },
             {
-                title: 'Pausa',
+                title: 'LEDWALL SHOW - Hålo',
+                time: '20.30',
+                important: false,
+                highlight: false
+            },
+            {
+                title: 'Ripresa partite',
                 time: '21.00',
                 important: false,
                 highlight: false
             },
             {
-                title: 'Playoff Maschili',
-                time: '21.40',
-                important: true,
+                title: 'Dj Ema',
+                time: '23.00',
+                important: false,
                 highlight: true
             },
             {
-                title: 'Fine partite',
-                time: '00.20',
-                important: false,
-                highlight: false
-            },
-            {
-                title: 'Fanum is back - MY8 qP',
-                time: '00.20',
+                title: 'Dj My8',
+                time: '00.00',
                 important: false,
                 highlight: true
             },
             {
                 title: 'Chiusura',
-                time: '1.30',
+                time: '1.00',
                 important: false,
                 highlight: true
             }
@@ -274,68 +160,43 @@ export default function Programma() {
             },
             {
                 title: 'Ottavi e Quarti di finale maschile e femminile',
-                time: '10.00',
-                important: false,
-                highlight: false
-            },
-            {
-                title: 'Pausa pranzo',
-                time: '14.00',
+                time: '12.00',
                 important: false,
                 highlight: false
             },
             {
                 title: 'Quarti di finale maschile',
-                time: '15.40',
-                important: false,
-                highlight: false
-            },
-            {
-                title: 'Pausa',
-                // title: 'Happy hour orangione',
-                time: '17.00',
+                time: '16.40',
                 important: false,
                 highlight: false
             },
             {
                 title: 'Semifinali maschile e femminile',
-                time: '18.00',
+                time: '18.40',
                 important: false,
                 highlight: false
             },
             {
-                title: 'Esibizione ballo Life Rovereto A.S.D',
-                time: '19.20',
+                title: 'Life Rovereto A.S.D',
+                time: '20.00',
                 important: false,
                 highlight: true
             },
             {
-                title: 'Pausa',
-                time: '20.20',
-                important: false,
-                highlight: false
-            },
-            {
                 title: 'Terzi e quarti maschile e femminile',
-                time: '21.00',
+                time: '20.40',
                 important: false,
                 highlight: false
             },
-            // {
-            //     title: 'Pausa',
-            //     time: '22.00',
-            //     important: false,
-            //     highlight: false
-            // },
             {
                 title: 'Finale femminile',
-                time: '22.00',
+                time: '21.40',
                 important: true,
                 highlight: true
             },
             {
                 title: 'Finale maschile',
-                time: '22.30',
+                time: '22.10',
                 important: true,
                 highlight: true
             },
@@ -345,12 +206,6 @@ export default function Programma() {
                 important: false,
                 highlight: false,
                 bold: true
-            },
-            {
-                title: 'Villa People X Ebol4',
-                time: '24.00',
-                important: false,
-                highlight: true
             },
             {
                 title: 'Chiusura',
